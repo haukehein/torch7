65c1b6b7c74162388cef56e25842e9048aacd7fd
haukehein <BitCrusher@gmx-topmail.de>
2016-09-22 16:15:48 +0200

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+[ -z "$( which mktemp     2> /dev/null )" ]  &&  echo "ERROR: 'mktemp' is not installed!"                &&  exit 1
 [ -z "$( which git        2> /dev/null )" ]  &&  echo "ERROR: 'git' is not installed!"                   &&  exit 1
 [ -z "$( which docker     2> /dev/null )" ]  &&  echo "ERROR: 'docker' is not installed!"                &&  exit 1
 [ -z "$( docker --version 2> /dev/null )" ]  &&  echo "ERROR: You are not allowed to execute 'docker'!"  &&  exit 1
